83c71ec29f10f9f6a794bb60cd03b6db24d38890
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-11-13 10:36:45 -0500

    need to add installation requirements

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-DIST = "storage/emulated/0/Music"
-echo $DIST 
-adb shell 'cd ${DIST} && for f in *\ *; do mv "$f" "${f// /_}"; done'
-python3 adb-sync.py --reverse "storage/emulated/0/Music/University_of_North_Texas_One_O'Clock_Lab_Band_Radio" ~/Music
+SOMETHING="storage/emulated/0/Music"
+#echo $DIST 
+adb shell 'cd storage/emulated/0/Music && for f in *\ *; do mv "$f" "${f// /_}"; done'
+python3 adb-sync.py --reverse "${SOMETHING}/University_of_North_Texas_One_O'Clock_Lab_Band_Radio" ~/Music
+adb shell "cd $SOMETHING && rm -rf University_Of*"
